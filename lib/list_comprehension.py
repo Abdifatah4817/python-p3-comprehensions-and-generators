4ce58fe8d100a971602d0f52e20e602986e9ee45
Abdifatah <mursalabdifatah17@gmail.com>
2025-11-18 12:04:53 +0300

    python-p3-comprehensions-and-generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [num for num in num_list if num % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+
+
+# Example usage:
+numbers = [0,1,2,3,4,5,6,7,8]
+print(return_evens(numbers)) 
+# Output: [0, 2, 4, 6, 8] 
+
+sentences = ["Hello", "I'm doing great", "Python is fun"]
+print(make_exclamation(sentences))
+# Output: ['Hello!', "I'm doing great!", 'Python is fun!']
